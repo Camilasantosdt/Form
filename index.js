736fd3331b599fa0629c43ad447ab091f4736f00
Camilasantosdt <cs607073@gmail.com>
2021-10-29 19:33:57 -0300

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function validarform(){
+
+    let nome = document.getElementById("nomecompleto");
+    let em=document.getElementById("em");
+    let cd=document.getElementById("cd");
+    let inicio=document.getElementById("inicio");
+    let text=document.getElementById("text");
+}
